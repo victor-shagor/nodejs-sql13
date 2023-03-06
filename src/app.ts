e0d21f5c5edd9af2b560a5135d501383ca8cc31f
victor-shagor <sadeoye@extracker.com>
2023-03-06 09:52:07 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const HOST = process.env.HOST || 'http://localhost';
+const PORT = parseInt(process.env.PORT || '4500');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World!' });
+});
+
+
+app.listen(PORT, async () => {
+
+  console.log(`Application started on URL ${HOST}:${PORT} 🎉`);
+});
